7a4ef30fe5e5d5d79354decf56d018c7472efa1a
M J Harvey <m.j.harvey@imperial.ac.uk>
2017-08-01 10:31:55 +0100

    new cx2 submit hook
    queue switcher

--- cx2/submit.py
+++ cx2/submit.py
@@ -7,15 +7,17 @@
 import sys
 import pbs
 
-list_of_resources = ["ncpus","mem","mpiprocs","ompthreads"]
+list_of_resources = ["ncpus","mem","mpiprocs","ompthreads", "cpumodel"]
 
-geometries = {16:range(1,5),24:range(1,21),28:range(1,16)}
+#geometries = {16:range(2,19),24: range( 18, 73, 6 ), 28:range( 18,73,6 ) } 
 
 try:
 #
 # Verify user requested walltime
    if ( pbs.event().job.Resource_List["walltime"] == None ):
       pbs.event().reject("Walltime must be specified, jobs cannot be scheduled without a runtime estimate!")
+
+   walltime = pbs.event().job.Resource_List["walltime"] 
 #
 # Verify a select statement was used
    if ( pbs.event().job.Resource_List["select"] == None ):
@@ -26,11 +28,11 @@ try:
       queue_name=pbs.event().job.queue.name
 #
 # Verify resources requested, count the number of nodes used, the ncpus requested and compute the number of IRUs
-   if queue_name[0] != "R":
+   if queue_name[0] != "R": # Anything goes in a Reservation
       nodect = 0
       chunks = repr(pbs.event().job.Resource_List["select"]).split("+")
       if ( len(chunks) != 1 ):
-         pbs.event().reject("You can only request one chunk of the form #PBS -l select=X:ncpus=Y:mpiprocs=Z:ompthreads=T.")
+         pbs.event().reject("You can only request one chunk of the form #PBS -l select=N:ncpus=X:mem=Ygb:mpiprocs=Z:ompthreads=W")
       for chunk in chunks:
          nodect+=int(chunk.split(":")[0])
          for rs in chunk.split(":")[1:]:
@@ -38,23 +40,25 @@ try:
             if ( kw not in list_of_resources ):
                pbs.event().reject("Select statements can only contain the resources: "+", ".join(list_of_resources))
             if ( kw == "ncpus" ):
-               node_size=int(rs.split("=")[1])
-      iru = nodect/18.0  
-      if ( ( node_size == 24 ) and ( nodect < 18 ) ):
-         model=":cpumodel=24s"
-         pbs.event().job.queue=pbs.server().queue("short")
-      elif ( ( node_size == 16 ) and ( iru in geometries[16] ) ):
-         model=":cpumodel=16"
-         pbs.event().job.queue=pbs.server().queue("general")
-      elif ( ( node_size == 24 ) and ( iru in geometries[24] ) ):
-         model=":cpumodel=24"
-         pbs.event().job.queue=pbs.server().queue("general")
-      elif ( ( node_size == 28 ) and ( iru in geometries[28] ) ):
-         model=":cpumodel=28"
-         pbs.event().job.queue=pbs.server().queue("general")
-      else:
-         pbs.event().reject("Unsupported geometry. Please read https://wiki.imperial.ac.uk/display/HPC/Job+sizing+on+cx2")
-      pbs.event().job.Resource_List["select"]=pbs.select(chunk+model)
+               ncpus=int(rs.split("=")[1])
+
+      matched = False
+      if nodect <= 18 and ncpus==24 and walltime <= pbs.duration("2:0:0"):
+          pbs.event().job.queue=pbs.server().queue("short")
+          matched = True 
+      if not matched and nodect>=2 and nodect <=18 and ncpus == 16 and walltime <= pbs.duration("72:0:0"):
+          pbs.event().job.queue=pbs.server().queue("general")
+          matched = True 
+      if not matched and nodect>=18 and nodect <=72 and ncpus in [24,28] and walltime <= pbs.duration("48:0:0"):
+          pbs.event().job.queue=pbs.server().queue("large")
+          matched = True 
+      if not matched and nodect>=72 and nodect <=270 and ncpus in [28] and walltime <= pbs.duration("24:0:0"):
+          pbs.event().job.queue=pbs.server().queue("capability")
+          matched = True 
+
+      if not matched:
+           pbs.event().reject("Unsupported geometry: Please read https://wiki.imperial.ac.uk/display/HPC/Job+sizing+on+cx2")
+
 #
 # If we accept or reject we are done
 except SystemExit:
